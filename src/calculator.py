c3cfdfa824399494a112fe4f41d6b81fbf527950
JulianoQueiroz <juliano.invest@outlook.com>
2021-07-26 11:51:43 -0300

    Funções de implementação logica da calculadora

--- src/calculator.py
+++ src/calculator.py
@@ -6,6 +6,8 @@ class MainWindow(qtw.QWidget):              # Janela Principal
         self.setWindowTitle('Calculadora')
         self.setLayout(qtw.QVBoxLayout())
         self.keypad()
+        self.temp_nums = []
+        self.fin_nums = []
         
         self.show()
     def keypad(self):                       # Container Vazio
@@ -13,28 +15,28 @@ class MainWindow(qtw.QWidget):              # Janela Principal
         container.setLayout(qtw.QGridLayout())
 
         # Botões 
-        result_field = qtw.QLineEdit()
-        btn_result = qtw.QPushButton('Digitar')
-        btn_clear = qtw.QPushButton('Limpar')
-        btn_9 = qtw.QPushButton('9')
-        btn_8 = qtw.QPushButton('8')
-        btn_7 = qtw.QPushButton('7')
-        btn_6 = qtw.QPushButton('6')
-        btn_5 = qtw.QPushButton('5')
-        btn_4 = qtw.QPushButton('4')
-        btn_3 = qtw.QPushButton('3')        # A ordem no escopo não importa
-        btn_2 = qtw.QPushButton('2')
-        btn_1 = qtw.QPushButton('1')
-        btn_0 = qtw.QPushButton('0')
-        btn_plus = qtw.QPushButton('+')
-        btn_mins = qtw.QPushButton('-')
-        btn_mult = qtw.QPushButton('*')
-        btn_divid = qtw.QPushButton('/')
+        self.result_field = qtw.QLineEdit()
+        btn_clear = qtw.QPushButton('Limpar', clicked = self.clear_calc)
+        btn_result = qtw.QPushButton('=', clicked = self.func_result)
+        btn_9 = qtw.QPushButton('9', clicked = lambda: self.num_press('9'))
+        btn_8 = qtw.QPushButton('8', clicked = lambda: self.num_press('8'))
+        btn_7 = qtw.QPushButton('7', clicked = lambda: self.num_press('7'))
+        btn_6 = qtw.QPushButton('6', clicked = lambda: self.num_press('6'))
+        btn_5 = qtw.QPushButton('5', clicked = lambda: self.num_press('5'))
+        btn_4 = qtw.QPushButton('4', clicked = lambda: self.num_press('4'))
+        btn_3 = qtw.QPushButton('3', clicked = lambda: self.num_press('3'))    # A ordem no escopo não importa
+        btn_2 = qtw.QPushButton('2', clicked = lambda: self.num_press('2'))
+        btn_1 = qtw.QPushButton('1', clicked = lambda: self.num_press('1'))
+        btn_0 = qtw.QPushButton('0', clicked = lambda: self.num_press('0'))
+        btn_plus = qtw.QPushButton('+', clicked = lambda: self.func_press('+'))
+        btn_mins = qtw.QPushButton('-', clicked = lambda: self.func_press('-'))
+        btn_mult = qtw.QPushButton('*', clicked = lambda: self.func_press('*'))
+        btn_divid = qtw.QPushButton('/', clicked = lambda: self.func_press('/'))
 
         # Adicionando os botões e a escala da calculadora para o Layout
-        container.layout().addWidget(result_field,0,0,1,4)
-        container.layout().addWidget(btn_result,1,0,1,2)
-        container.layout().addWidget(btn_clear,1,2,1,2)
+        container.layout().addWidget(self.result_field,0,0,1,4)
+        container.layout().addWidget(btn_clear,1,0,1,2) 
+        container.layout().addWidget(btn_result,1,2,1,2)
         container.layout().addWidget(btn_9,2,0)
         container.layout().addWidget(btn_8,2,1)
         container.layout().addWidget(btn_7,2,2)
@@ -50,7 +52,35 @@ class MainWindow(qtw.QWidget):              # Janela Principal
         container.layout().addWidget(btn_0,5,0,1,3)
         container.layout().addWidget(btn_divid,5,3)
         self.layout().addWidget(container) # Coloca a ferramenta no container principal
+    
+    def num_press(self, key_number): # função de input dos numeros dos botões 
+        self.temp_nums.append(key_number)
+        temp_string = ''.join(self.temp_nums)
+        if self.fin_nums:
+            self.result_field.setText(''.join(self.fin_nums) + temp_string)
+        else:
+            self.result_field.setText(temp_string)
+    
+    def func_press(self, operator):  # função de input dos operadores dos botões 
+        temp_string = ''.join(self.temp_nums)
+        self.fin_nums.append(temp_string)
+        self.fin_nums.append(operator)
+        self.temp_nums = []
+        self.result_field.setText(''.join(self.fin_nums))
+
+    def func_result(self):
+        fin_string = ''.join(self.fin_nums) + ''.join(self.temp_nums)
+        result_string = eval(fin_string)
+        fin_string += '='
+        fin_string += str(result_string)
+        self.result_field.setText(fin_string)
+
+    def clear_calc(self):
+        self.result_field.clear()
+        self.temp_nums = []
+        self.fin_nums = []
 
 app = qtw.QApplication([])
 mw = MainWindow()
+app.setStyle(qtw.QStyleFactory.create('Fusão'))
 app.exec_()
